25832b3b8f8b90bd3bc7507d8726e172903dd7e1
Jakke Korpelainen <jakke.korpelainen@op.fi>
2022-04-07 12:17:53 +0300

    Remake with react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react"
+import styled, { createGlobalStyle } from "styled-components"
+import { About } from "./About"
+import face from "./face.jpg"
+import { SplashImage, SplashWrapper } from "./SplashImage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <GlobalStyle />
+      <StyledApp>
+        <SplashWrapper>
+          <SplashImage image={face} />
+        </SplashWrapper>
+        <About />
+      </StyledApp>
+    </Fragment>
+  )
 }
 
-export default App;
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  #root {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+
+    font-family: "Karla", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  a {
+    color: teal;
+  }
+
+  html {
+    font-size: 22px;
+    line-height: 27px;
+    background-color: teal;
+  }
+
+  body {
+    font-family: "Karla", sans-serif;
+    font-size: 1rem;
+    line-height: 1.5rem;
+  }
+
+  h1,h2,h3,h4,h5,h6 {
+    font-family: "Karla", sans-serif;
+  }
+
+  p,
+  ul,
+  ol,
+  pre,
+  table,
+  blockquote {
+    margin-top: 0rem;
+    margin-bottom: 1.5rem;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export default App
