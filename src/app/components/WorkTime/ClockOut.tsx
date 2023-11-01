32a8a99b4ba7f859c3ad48d5fe411d21ea4616a8
Jakke Korpelainen <jakke.korpelainen@loihde.com>
2023-11-01 12:08:56 +0200

    Improve support for mobile

--- src/app/components/WorkTime/ClockOut.tsx
+++ src/app/components/WorkTime/ClockOut.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import clsx from "clsx";
-import { format, isPast } from "date-fns";
+import { format, formatDistanceStrict, isPast } from "date-fns";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 import { calculateClockOut } from "./helper";
 import { TimeState } from "./reducer/types";
 
 export const ClockOut = ({ ...state }: TimeState) => {
+  "use client";
+  const [currentTime, setCurrentTime] = useState<Date | null>(null);
+
   let clockOutValue = "N/A";
   let clockOutDate = null;
 
@@ -18,6 +22,13 @@ export const ClockOut = ({ ...state }: TimeState) => {
     console.error(err);
   }
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   const isPastClockOutDate = clockOutDate ? isPast(clockOutDate) : false;
   const leaveText = isPastClockOutDate ? "Time to leave" : "You can leave at";
 
@@ -26,16 +37,26 @@ export const ClockOut = ({ ...state }: TimeState) => {
       <Image
         className={clsx(
           { ["animate-bounce"]: isPastClockOutDate },
-          "bottom-15 absolute left-20 lg:bottom-20 lg:left-40",
+          "bottom-15 absolute left-[6rem] md:bottom-10 md:left-40 lg:bottom-20 lg:left-40",
         )}
         alt="Aggressive Goose looking at the time"
         width={900}
         height={900}
         src="/hanh.png"
       />
-      <div className="z-10">
+      <div className="z-10 flex flex-col gap-5">
         <p>{leaveText}</p>
-        <h1>{clockOutValue}</h1>
+        <h1 className="lg:text-7xl mb-0 leading-[2rem] lg:text-[6rem] lg:leading-[7rem]">{clockOutValue}</h1>
+
+        {!isPastClockOutDate && currentTime && clockOutDate && (
+          <p>
+            {formatDistanceStrict(clockOutDate, currentTime, {
+              addSuffix: true,
+              unit: "second",
+            })}
+            .
+          </p>
+        )}
       </div>
     </div>
   );
--- src/app/components/WorkTime/index.tsx
+++ src/app/components/WorkTime/index.tsx
@@ -21,47 +21,50 @@ export const WorkTime = () => {
   const pauses = [...state.pauses].sort((a, b) => sortDateDesc(a.startTime, b.startTime));
 
   return (
-    <div className="flex grow flex-col text-white lg:flex-row">
-      <Column className={clsx(columnClass, "bg-black")}>
-        <p>For Anni</p>
-        <h1 className="sticky top-0 z-10 mb-2 bg-black py-5">Work Time Calculator</h1>
+    <>
+      <div className="flex grow flex-col text-white lg:flex-row">
+        <Column className={clsx(columnClass, "grow bg-black lg:mr-[50%]")}>
+          <p>For Anni</p>
 
-        <h3>Work Started</h3>
-        <TimeInput id={"work"} startTime={state.startTime} onStartChange={onStartChange} />
+          <h1 className="z-10 mb-2 py-2 lg:py-5">Work Time Calculator</h1>
 
-        {/* breaks */}
-        {pauses.map((pause, index) => (
-          <div className="border-1 relative rounded border border-orange-900 p-5" key={pause.id}>
-            <h3 className="mb-5">
-              {pause.paid ? "Paid break" : "Unpaid break"} {index + 1}
-            </h3>
-            <TimeInput
-              id={pause.id}
-              key={pause.id}
-              startTime={pause.startTime}
-              endTime={pause.endTime}
-              paid={pause.paid}
-              onDelete={() => dispatch({ type: "DELETE_PAUSE", id: pause.id })}
-              onStartChange={(value) => dispatch({ type: "SET_PAUSE_START_TIME", id: pause.id, value })}
-              onEndChange={(value) => dispatch({ type: "SET_PAUSE_END_TIME", id: pause.id, value })}
-              onPaidChange={(value) => dispatch({ type: "SET_PAUSE_PAID", id: pause.id, value })}
-            />
+          <h3>Work Started</h3>
+          <TimeInput id={"work"} startTime={state.startTime} onStartChange={onStartChange} />
+
+          {/* breaks */}
+          {pauses.map((pause, index) => (
+            <div className="border-1 relative rounded border border-orange-900 p-5" key={pause.id}>
+              <h3 className="mb-5">
+                {pause.paid ? "Paid break #" : "Unpaid break #"} {index + 1}
+              </h3>
+              <TimeInput
+                id={pause.id}
+                key={pause.id}
+                startTime={pause.startTime}
+                endTime={pause.endTime}
+                paid={pause.paid}
+                onDelete={() => dispatch({ type: "DELETE_PAUSE", id: pause.id })}
+                onStartChange={(value) => dispatch({ type: "SET_PAUSE_START_TIME", id: pause.id, value })}
+                onEndChange={(value) => dispatch({ type: "SET_PAUSE_END_TIME", id: pause.id, value })}
+                onPaidChange={(value) => dispatch({ type: "SET_PAUSE_PAID", id: pause.id, value })}
+              />
+            </div>
+          ))}
+          <div className={clsx({ ["mt-10"]: pauses.length > 0 })}>
+            <button className={buttonClass} type="button" onClick={() => dispatch({ type: "ADD_PAUSE" })}>
+              ➕ Add break
+            </button>
           </div>
-        ))}
-        <div className={clsx({ ["mt-10"]: pauses.length > 0 })}>
-          <button className={buttonClass} type="button" onClick={() => dispatch({ type: "ADD_PAUSE" })}>
-            Add break
-          </button>
-        </div>
-      </Column>
-      <Column
-        className={clsx(
-          columnClass,
-          "order-first items-center justify-center bg-orange-900 lg:fixed lg:right-0 lg:order-none lg:h-full",
-        )}
-      >
-        <ClockOut {...state} />
-      </Column>
-    </div>
+        </Column>
+        <Column
+          className={clsx(
+            columnClass,
+            "lg:min-h-auto order-first min-h-[20vh] items-center justify-center bg-orange-900 lg:static lg:fixed lg:right-0 lg:order-none lg:h-full",
+          )}
+        >
+          <ClockOut {...state} />
+        </Column>
+      </div>
+    </>
   );
 };
