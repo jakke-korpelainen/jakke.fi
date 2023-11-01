be870d2d4176b45afd39b59f70176d4e625160b0
Jakke Korpelainen <jakke.korpelainen@loihde.com>
2023-11-01 14:09:42 +0200

    Adjust work time for mobile devices

--- src/app/components/WorkTime/ClockOut.tsx
+++ src/app/components/WorkTime/ClockOut.tsx
@@ -33,18 +33,18 @@ export const ClockOut = ({ ...state }: TimeState) => {
   const leaveText = isPastClockOutDate ? "Time to leave" : "You can leave at";
 
   return (
-    <div className="relative">
+    <div className="relative flex w-full grow flex-col items-center overflow-hidden">
       <Image
         className={clsx(
           { ["animate-bounce"]: isPastClockOutDate },
-          "bottom-15 absolute left-[6rem] md:bottom-10 md:left-40 lg:bottom-20 lg:left-40",
+          "absolute -bottom-20 left-20 sm:-bottom-40 md:-bottom-[14rem] md:-left-16 md:scale-50 lg:bottom-[27vh] lg:left-56 lg:scale-100 xl:bottom-[27vh] xl:left-52",
         )}
         alt="Aggressive Goose looking at the time"
         width={900}
         height={900}
         src="/hanh.png"
       />
-      <div className="z-10 flex flex-col gap-5">
+      <div className="z-10 flex w-full grow flex-col justify-center gap-5">
         <p>{leaveText}</p>
         <h1 className="lg:text-7xl mb-0 leading-[2rem] lg:text-[6rem] lg:leading-[7rem]">{clockOutValue}</h1>
 
