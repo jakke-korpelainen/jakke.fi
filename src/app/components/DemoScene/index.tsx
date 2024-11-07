56f15639f6f4b531e0a821cb8bdc67dce0beee1b
Jakke Korpelainen <jakke.korpelainen@loihde.com>
2024-11-07 21:04:42 +0200

    refactor: tweak perf, etc

--- src/app/components/DemoScene/index.tsx
+++ src/app/components/DemoScene/index.tsx
@@ -2,60 +2,44 @@
 
 import { Physics } from "@react-three/cannon";
 import { Canvas } from "@react-three/fiber";
-import { EffectComposer, N8AO } from "@react-three/postprocessing";
-
 import { Cursor } from "./Cursor";
 import { InstancedSpheres } from "./InstancedSpheres";
-import { Plane } from "./Plane";
+import { Plane, TexturedPlane } from "./Plane";
+import { EffectComposer, N8AO } from "@react-three/postprocessing";
+import { Color, Fog } from "three";
 
-export const colors = {
-  gray: "#333",
-  teal: "teal",
-  white: "white",
-  plane: {
-    top: "#16161d",
-    left: "#16161d",
-    right: "#16161d",
-    bottom: "#16161d",
-  },
+export const COLORS = {
+  cursor: new Color("orange"),
+  sphere: new Color("teal"),
+  plane: new Color("#16161d"),
 } as const;
 
-const imageSrc = "/face_2024.jpg";
+export const SURFACE_TEXTURE = "/face_2024.jpg";
 
 export default function DemoScene() {
   return (
-    <Canvas className="cursor-none select-none" dpr={[0.5, 1]} shadows camera={{ position: [0, 0, 8] }}>
+    <Canvas className="cursor-none select-none" performance={{ min: 0.25, max: 2 }} camera={{ position: [0, 0, 8] }}>
       <EffectComposer>
-        <N8AO quality="performance" halfRes color={colors.gray} aoRadius={5} distanceFalloff={5} intensity={5} />
+        <N8AO quality="performance" halfRes />
       </EffectComposer>
       <hemisphereLight intensity={2.5} />
-      <spotLight
-        position={[0, 0, 30]}
-        angle={0.3}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize-width={256}
-        shadow-mapSize-height={256}
-      />
-      <pointLight position={[0, 0, -30]} intensity={0.5} />
       <Physics gravity={[0, 0, -30]}>
         <Cursor />
 
         {/* top */}
-        <Plane color={colors.plane.top} position={[0, 6, 0]} rotation={[0.9, 0, 0]} />
+        <Plane color={COLORS.plane} position={[0, 6, 0]} rotation={[0.9, 0, 0]} />
 
         {/* left */}
-        <Plane color={colors.plane.left} position={[-6, 0, 0]} rotation={[0, 0.9, 0]} />
+        <Plane color={COLORS.plane} position={[-6, 0, 0]} rotation={[0, 0.9, 0]} />
 
         {/* surface */}
-        <Plane image={imageSrc} position={[0, 0, 0]} rotation={[0, 0, 0]} />
+        <TexturedPlane textureSrc={SURFACE_TEXTURE} position={[0, 0, 0]} rotation={[0, 0, 0]} />
 
         {/* right */}
-        <Plane color={colors.plane.right} position={[6, 0, 0]} rotation={[0, -0.9, 0]} />
+        <Plane color={COLORS.plane} position={[6, 0, 0]} rotation={[0, -0.9, 0]} />
 
         {/* bottom */}
-        <Plane color={colors.plane.bottom} position={[0, -6, 0]} rotation={[-0.9, 0, 0]} />
+        <Plane color={COLORS.plane} position={[0, -6, 0]} rotation={[-0.9, 0, 0]} />
         <InstancedSpheres />
       </Physics>
     </Canvas>
