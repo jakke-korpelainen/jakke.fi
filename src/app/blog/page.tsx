2ea7375710ff09fa665f10b1a3113a0dfcf3576d
Jakke Korpelainen <jakke.korpelainen@loihde.com>
2024-11-09 11:00:16 +0200

    feat: add breadcrumb, fix styling issues

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 
 import { BlogPostList } from "@/components/Blog/BlogPostList";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
+import { Breadcrumb } from "@/components/Breadcrumb";
 
 export const metadata = {
   title: "Blog Articles - jakke.fi",
@@ -16,8 +17,11 @@ interface BlogParams {
 export default async function BlogIndex({ searchParams }: BlogParams) {
   const { tag } = searchParams ?? {};
   return (
-    <div>
+    <div className="space-y-10">
+      <Breadcrumb items={[{ label: "Location" }, { label: "Blog" }]} />
       <h1>Blog</h1>
+      <h2>Articles</h2>
+
       <Suspense fallback={<LoadingSpinner />}>
         <BlogPostList tag={tag} />
       </Suspense>
