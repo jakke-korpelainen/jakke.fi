ffd31ef20130f98364cb80273c3b194adec01c4d
jwekke <3626319+jakke-korpelainen@users.noreply.github.com>
2025-10-19 15:14:37 +0300

    fix: prefer Link over anchor

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,7 +7,7 @@ export default function Error() {
   return (
     <div className="flex w-full grow flex-col space-y-10 bg-gradient-to-b from-[#121217] to-[#262a2b] p-5 text-white sm:p-10">
       <header className="container mx-auto">
-        <a href="/" className="text-2xl text-white no-underline hover:text-white">
+        <Link href="/" className="text-2xl text-white no-underline hover:text-white">
           <div className="flex gap-5">
             <Image sizes="" className="object-contain" src="/ball.png" alt="Jakke.fi logo" width={64} height={64} />
             <div>
@@ -15,7 +15,7 @@ export default function Error() {
               <p className="text-sm sm:text-lg">Error</p>
             </div>
           </div>
-        </a>
+        </Link>
       </header>
       <div className="container mx-auto space-y-10">
         <h1>
