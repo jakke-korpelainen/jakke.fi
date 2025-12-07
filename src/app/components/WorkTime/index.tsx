3c3336a6196a52e391b57d3931f9cece2534bdc2
Jakke Korpelainen <3626319+jakke-korpelainen@users.noreply.github.com>
2025-12-07 17:54:19 +0200

    Remove redundant fragment

--- src/app/components/WorkTime/index.tsx
+++ src/app/components/WorkTime/index.tsx
@@ -21,50 +21,48 @@ export const WorkTime = () => {
   const pauses = [...state.pauses].sort((a, b) => sortDateDesc(a.startTime, b.startTime));
 
   return (
-    <>
-      <div className="flex grow flex-col text-white lg:flex-row">
-        <Column className={clsx(columnClass, "grow bg-black lg:mr-[50%]")}>
-          <p>For Anni</p>
+    <div className="flex grow flex-col text-white lg:flex-row">
+      <Column className={clsx(columnClass, "grow bg-black lg:mr-[50%]")}>
+        <p>For Anni</p>
 
-          <h1 className="z-10 mb-2 py-2 lg:py-5">Work Time Calculator</h1>
+        <h1 className="z-10 mb-2 py-2 lg:py-5">Work Time Calculator</h1>
 
-          <h3>Work Started</h3>
-          <TimeInput id={"work"} startTime={state.startTime} onStartChange={onStartChange} />
+        <h3>Work Started</h3>
+        <TimeInput id={"work"} startTime={state.startTime} onStartChange={onStartChange} />
 
-          {/* breaks */}
-          {pauses.map((pause, index) => (
-            <div className="border-1 relative rounded border border-orange-900 p-5" key={pause.id}>
-              <h3 className="mb-5">
-                {pause.paid ? "Paid break #" : "Unpaid break #"} {index + 1}
-              </h3>
-              <TimeInput
-                id={pause.id}
-                key={pause.id}
-                startTime={pause.startTime}
-                endTime={pause.endTime}
-                paid={pause.paid}
-                onDelete={() => dispatch({ type: "DELETE_PAUSE", id: pause.id })}
-                onStartChange={(value) => dispatch({ type: "SET_PAUSE_START_TIME", id: pause.id, value })}
-                onEndChange={(value) => dispatch({ type: "SET_PAUSE_END_TIME", id: pause.id, value })}
-                onPaidChange={(value) => dispatch({ type: "SET_PAUSE_PAID", id: pause.id, value })}
-              />
-            </div>
-          ))}
-          <div className={clsx({ ["mt-10"]: pauses.length > 0 })}>
-            <button className={buttonClass} type="button" onClick={() => dispatch({ type: "ADD_PAUSE" })}>
-              ➕ Add break
-            </button>
+        {/* breaks */}
+        {pauses.map((pause, index) => (
+          <div className="border-1 relative rounded border border-orange-900 p-5" key={pause.id}>
+            <h3 className="mb-5">
+              {pause.paid ? "Paid break #" : "Unpaid break #"} {index + 1}
+            </h3>
+            <TimeInput
+              id={pause.id}
+              key={pause.id}
+              startTime={pause.startTime}
+              endTime={pause.endTime}
+              paid={pause.paid}
+              onDelete={() => dispatch({ type: "DELETE_PAUSE", id: pause.id })}
+              onStartChange={(value) => dispatch({ type: "SET_PAUSE_START_TIME", id: pause.id, value })}
+              onEndChange={(value) => dispatch({ type: "SET_PAUSE_END_TIME", id: pause.id, value })}
+              onPaidChange={(value) => dispatch({ type: "SET_PAUSE_PAID", id: pause.id, value })}
+            />
           </div>
-        </Column>
-        <Column
-          className={clsx(
-            columnClass,
-            "lg:min-h-auto order-first min-h-[20vh] items-center justify-center bg-orange-900 lg:static lg:fixed lg:right-0 lg:order-none lg:h-full",
-          )}
-        >
-          <ClockOut {...state} />
-        </Column>
-      </div>
-    </>
+        ))}
+        <div className={clsx({ ["mt-10"]: pauses.length > 0 })}>
+          <button className={buttonClass} type="button" onClick={() => dispatch({ type: "ADD_PAUSE" })}>
+            ➕ Add break
+          </button>
+        </div>
+      </Column>
+      <Column
+        className={clsx(
+          columnClass,
+          "lg:min-h-auto order-first min-h-[20vh] items-center justify-center bg-orange-900 lg:static lg:fixed lg:right-0 lg:order-none lg:h-full",
+        )}
+      >
+        <ClockOut {...state} />
+      </Column>
+    </div>
   );
 };
