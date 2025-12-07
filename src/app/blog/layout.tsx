355ac709e3a2a83ebfd0bd1c8c290090a291b884
Jakke Korpelainen <3626319+jakke-korpelainen@users.noreply.github.com>
2025-12-07 17:54:20 +0200

    style: footer mobile improvements

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -19,7 +19,7 @@ export default function BlogLayout({ children }: { children: React.ReactNode }) 
       </header>
       <div className="flex grow flex-col justify-between">
         <div className="container mx-auto grow">{children}</div>
-        <div className="container mx-auto flex flex-col items-center justify-between gap-10 sm:flex-row">
+        <div className="container mx-auto flex flex-col-reverse justify-between gap-10 sm:flex-row">
           <div className="select-none text-gray-400">&copy; jakke.fi</div>
           <ul className="flex flex-col gap-5 sm:inline-flex sm:flex-row [&>li>a]:no-underline [&>li]:text-base">
             <li>
